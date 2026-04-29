abc2cc83b36405e3cf629bace3999f4231ddd5a6
hassanoraimo1-glitch <hassanoraimo1@gmail.com>
2026-04-29 07:25:47 +0300

    Add files via upload

--- src/utils/format.js
+++ src/utils/format.js
@@ -19,11 +19,7 @@ export function fmtDate(d) {
 }
 
 export function todayStr() {
-  const d = new Date();
-  const year = d.getFullYear();
-  const month = String(d.getMonth() + 1).padStart(2, '0');
-  const day = String(d.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
+  return fmtDate(new Date());
 }
 
 export function fmtEGP(n) {
